db03d6fb8030e8d9a89611c79750b704cd378876
Michael Scherbakow <ryupold@gmail.com>
2017-04-24 21:28:59 +0200

    #17 grid

--- Assets/Editor/NavigationGridEditor.cs
+++ Assets/Editor/NavigationGridEditor.cs
@@ -16,18 +16,47 @@ public class NavigationGridEditor : Editor
 
         grid = (NavigationGrid)target;
 
-        
+        if (GUILayout.Button("Recalculate Grid"))
+        {
+            grid.RecalculateGrid(grid.extend.Value);
+        }
     }
 
     [DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
     static void DrawGameObjectName(Transform transform, GizmoType gizmoType)
     {
-        if ((gizmoType & GizmoType.NotInSelectionHierarchy) != 0)
+        if (!grid || (gizmoType & GizmoType.NotInSelectionHierarchy) != 0)
         {
             return;
         }
 
-        if (grid && grid.gameObject == transform.gameObject)
-            Handles.Label(transform.position, transform.gameObject.name);
+        DrawFaceString(CubeFace.Up);
+        DrawFaceString(CubeFace.Down);
+        DrawFaceString(CubeFace.Left);
+        DrawFaceString(CubeFace.Right);
+        DrawFaceString(CubeFace.Forward);
+        DrawFaceString(CubeFace.Back);
+
+        var coordinateStyle = new GUIStyle();
+        coordinateStyle.normal.textColor = Color.red;
+        coordinateStyle.fontSize = 10;
+        
+        
+        foreach (var gf in grid.GridFields)
+        {
+            if(gf.Key != null && gf.Value != null) Handles.Label(gf.Value, gf.Key.ToString(), coordinateStyle);
+        }
+    }
+
+    private static void DrawFaceString(CubeFace face)
+    {
+        var faceStyle = new GUIStyle();
+        faceStyle.normal.textColor = grid.labelColor;
+        faceStyle.fontSize = 15;
+        faceStyle.fontStyle = FontStyle.Bold;
+
+        //(CubeFace.Right.ToUnitVector()*grid.extend.Value/2f)
+        var pos = face.ToUnitVector() * (grid.extend.Value/2f + grid.labelDistance) + grid.offset + grid.labelOffset;
+        Handles.Label(pos, face.ToString(), faceStyle);
     }
 }
--- Assets/Scripts/NavigationGrid.cs
+++ Assets/Scripts/NavigationGrid.cs
@@ -1,33 +1,98 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Assets.Utils;
 using UniRx;
 using UnityEngine;
 
 public class NavigationGrid : MonoBehaviour
 {
-    public int size = 100;
+    public short size = 10;
+
     public FloatReactiveProperty extend = new FloatReactiveProperty(10);
 
     public float padding = 0.2f;
 
     [SerializeField]
     private float fieldSize;
-    
+
     [SerializeField]
     private float faceSize;
 
+    private readonly Dictionary<Position, Vector3> grid = new Dictionary<Position, Vector3>();
+    public KeyValuePair<Position, Vector3>[] GridFields { get { return grid.ToArray(); } }
 
     public Vector3 offset = Vector3.zero;
+    public Vector3 labelOffset = Vector3.zero;
+
+    [Range(0f, 100f)]
+    public float labelDistance = 5f;
+
+    public Color labelColor = Color.blue;
+    public bool showGrid = true;
+
+
 
     void Start()
     {
-        extend.Throttle(TimeSpan.FromSeconds(1)).Subscribe(e => RecalculateGrid()).AddTo(this);
+        IoC.RegisterSingleton(this);
+
+        extend
+        .Where(x => x > 0f)
+        .Throttle(TimeSpan.FromSeconds(1))
+        .LogOnNext("recalculating grid for new extend: {0}")
+        .Subscribe(RecalculateGrid).AddTo(this);
     }
 
-    private void RecalculateGrid(){
-        faceSize = extend.Value * 2f;
-        fieldSize = faceSize / 5;
+    public void RecalculateGrid(float extend)
+    {
+        offset = Vector3.up * extend / 2f;
+        faceSize = (extend) * 2f;
+        fieldSize = faceSize / size;
+        labelOffset = new Vector3(faceSize / 3, 0, -faceSize / 12);
+
+        grid.Clear();
+        AddCubeFacePositions(CubeFace.Up);
+        AddCubeFacePositions(CubeFace.Down);
+        AddCubeFacePositions(CubeFace.Left);
+        AddCubeFacePositions(CubeFace.Right);
+        AddCubeFacePositions(CubeFace.Forward);
+        AddCubeFacePositions(CubeFace.Back);
+
+        Debug.Log("Grid created: Range:(" + new Position(grid.Min(x => x.Key.Combined), CubeFace.Up) + ") -> (" + new Position(grid.Max(x => x.Key.Combined), CubeFace.Up) + ")");
+    }
+
+    private void AddCubeFacePositions(CubeFace face)
+    {
+        var center = transform.position + face.ToUnitVector() * extend.Value / 2f + Vector3.up * extend.Value / 2f;
+        var upperLeft = center
+        + face.ToUpperLeftUnitVector() * (Mathf.Sqrt(extend.Value * extend.Value + extend.Value * extend.Value) / 2f) // upper left
+        //- new Vector3(fieldSize/2f, 0, fieldSize/2f); // center field
+        + face.ToUpperLeftUnitVector() * (-Mathf.Sqrt(fieldSize * fieldSize + fieldSize * fieldSize) / 4f) // center field
+        ;
+
+        var fieldDirection = -face.ToUpperLeftUnitVector() * (Mathf.Sqrt(fieldSize * fieldSize + fieldSize * fieldSize) / 2f);
+
+        var start = (short)((int)face * size);
+        for (short x = start; x < start + size; x++)
+        {
+            for (short y = 0; y < size; y++)
+            {
+                var nX = x - start;
+                var nY = y;
+
+                var fieldOffset = Vector3.zero;
+                if (Mathf.Approximately(fieldDirection.y, 0f))
+                    fieldOffset = new Vector3(fieldDirection.x * nX, 0f, fieldDirection.z * nY);
+                if (Mathf.Approximately(fieldDirection.x, 0f))
+                    fieldOffset = new Vector3(0f, fieldDirection.y * nX, fieldDirection.z * nY);
+                if (Mathf.Approximately(fieldDirection.z, 0f))
+                    fieldOffset = new Vector3(fieldDirection.x * nX, fieldDirection.y * nY, 0f);
+
+                grid.Add(new Position(x, y, face), upperLeft + fieldOffset);
+            }
+        }
     }
 
     void OnDrawGizmosSelected()
@@ -37,12 +102,133 @@ public class NavigationGrid : MonoBehaviour
 
     private void DrawGizmos()
     {
-        offset = Vector3.up * extend.Value / 2f;
         Gizmos.color = new Color(1f, 1f, 1f, 1f);
         Gizmos.DrawWireCube(transform.position + offset, new Vector3(extend.Value + padding, extend.Value + padding, extend.Value + padding));
-        
-        
-        //top
-        Gizmos.DrawWireCube(transform.position + offset, new Vector3(extend.Value + padding, extend.Value + padding, extend.Value + padding));
+
+        if(!showGrid) return;
+        Gizmos.color = new Color(1f, 0f, 0f, 1f);
+        foreach (var g in grid)
+        {
+            //Gizmos.DrawWireCube(g.Value + Vector3.up * padding / 2f, new Vector3(fieldSize / 2f, padding, fieldSize / 2f));
+
+            switch (g.Key.face)
+            {
+                case CubeFace.Up:
+                case CubeFace.Down:
+                    Gizmos.DrawWireCube(g.Value + g.Key.face.ToUnitVector() * padding / 2f, new Vector3(fieldSize / 2f, padding, fieldSize / 2f));
+                    break;
+                case CubeFace.Left:
+                case CubeFace.Right:
+                    Gizmos.DrawWireCube(g.Value + g.Key.face.ToUnitVector() * padding / 2f, new Vector3(padding, fieldSize / 2f, fieldSize / 2f));
+                    break;
+                case CubeFace.Forward:
+                case CubeFace.Back:
+                    Gizmos.DrawWireCube(g.Value + g.Key.face.ToUnitVector() * padding / 2f, new Vector3(fieldSize / 2f, fieldSize / 2f, padding));
+                    break;
+            }
+        }
+    }
+    public class Position
+    {
+        public readonly short x;
+        public readonly short y;
+
+        public readonly CubeFace face;
+
+        public Position(short x, short y, CubeFace face)
+        {
+            this.x = x;
+            this.y = y;
+            this.face = face;
+        }
+
+        public Position(int xAndyCombined, CubeFace face)
+        {
+            this.x = (short)(xAndyCombined >> 16);
+            this.y = (short)(xAndyCombined & 0xFFFF);
+            this.face = face;
+        }
+
+        public int Combined { get { return (x << 16) + y; } }
+
+        public override int GetHashCode()
+        {
+            return Combined;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return ((Position)obj).GetHashCode() == this.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+
+
+}
+
+public enum CubeFace
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Forward,
+    Back,
+}
+
+public static class CubeFaceExtensions
+{
+    public static Vector3 ToUnitVector(this CubeFace cubeFace)
+    {
+        switch (cubeFace)
+        {
+            case CubeFace.Up: return Vector3.up;
+            case CubeFace.Down: return Vector3.down;
+            case CubeFace.Left: return Vector3.left;
+            case CubeFace.Right: return Vector3.right;
+            case CubeFace.Forward: return Vector3.forward;
+            case CubeFace.Back: return Vector3.back;
+
+            default: throw new NotImplementedException("wtf òÓ");
+        }
+    }
+
+    public static Vector3 ToUpperLeftUnitVector(this CubeFace cubeFace)
+    {
+        var one = new Vector3(1, 1, 1);
+        switch (cubeFace)
+        {
+            case CubeFace.Up: return (cubeFace.ToUnitVector() - one).normalized;
+            case CubeFace.Down: return -CubeFace.Up.ToUpperLeftUnitVector();
+            case CubeFace.Right: return (cubeFace.ToUnitVector() - one).normalized;
+            case CubeFace.Left: return -CubeFace.Right.ToUpperLeftUnitVector();
+            case CubeFace.Forward: return (cubeFace.ToUnitVector() - one).normalized;
+            case CubeFace.Back: return -CubeFace.Forward.ToUpperLeftUnitVector();
+
+            default: throw new NotImplementedException("wtf òÓ");
+        }
+    }
+
+    public static CubeFace ToCubeFace(this Vector3 vector)
+    {
+        var v = vector.normalized;
+        var e = Mathf.Epsilon;
+
+        return
+            Mathf.Approximately(v.x, 0f) && v.y > e ? CubeFace.Up :
+            Mathf.Approximately(v.x, 0f) && v.y < e ? CubeFace.Down :
+            v.x < e && Mathf.Approximately(v.y, 0f) ? CubeFace.Left :
+            v.x > e && Mathf.Approximately(v.y, 0f) ? CubeFace.Right :
+            Mathf.Approximately(v.x, 0f) && Mathf.Approximately(v.y, 0f) && v.z > e ? CubeFace.Forward :
+            CubeFace.Back;
     }
 }
